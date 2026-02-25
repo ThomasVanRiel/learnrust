8f19159685a48812df3cb55fa110f3e21cad0382
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-02-25 15:14:18 +0100

    Implemented sorting and limitting

--- csvtool/src/main.rs
+++ csvtool/src/main.rs
@@ -32,18 +32,22 @@ struct Person {
     salary: u32,
 }
 
-impl Person {
-    fn print(&self) {
-        println!(
+impl std::fmt::Display for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // Format string from println! is "{:<20} {:>4} {:<12} {:>8}"
+        write!(
+            f,
             "{:<20} {:>4} {:<12} {:>8}",
             self.name, self.age, self.city, self.salary
-        );
+        )
     }
 }
 
 struct Config {
     filename: String,
     filter: Option<(String, FilterOp, String)>,
+    sort: Option<String>,
+    limit: Option<usize>,
 }
 
 impl Config {
@@ -81,6 +85,8 @@ impl Config {
 
     fn new(args: &[String]) -> Result<Config, String> {
         let filename = args.get(1);
+
+        // Check if filter flag is in args and get query
         let filter = if let Some(pos) = args.iter().position(|a| a.eq("--filter")) {
             if let Some(filter_string) = args.get(pos + 1) {
                 Config::build_filter(filter_string)
@@ -92,10 +98,38 @@ impl Config {
             None
         };
 
-        match (filename, filter) {
-            (Some(filename), filter) => Ok(Config {
+        // Check if sort flag is in args and get key
+        let sort = if let Some(pos) = args.iter().position(|a| a.eq("--sort")) {
+            if let Some(sort_key) = args.get(pos + 1) {
+                Some(sort_key.to_string())
+            } else {
+                println!("--sort flag specified but no key provided.");
+                None
+            }
+        } else {
+            None
+        };
+
+        // Check if limit flag is in args and get key
+        let limit = if let Some(pos) = args.iter().position(|a| a.eq("--limit")) {
+            if let Some(limit_string) = args.get(pos + 1) {
+                Some(limit_string.parse::<usize>().map_err(|e| {
+                    format!("Error: {e} while parsing string \"{limit_string}\" to usize")
+                })?)
+            } else {
+                println!("--limit flag specified but no amount specified.");
+                None
+            }
+        } else {
+            None
+        };
+
+        match (filename, filter, sort, limit) {
+            (Some(filename), filter, sort, limit) => Ok(Config {
                 filename: filename.to_string(),
                 filter,
+                sort,
+                limit,
             }),
             _ => Err(String::from(
                 "Usage: csvtool <file> [--filter heading=query]",
@@ -131,58 +165,53 @@ fn run(config: &Config) -> Result<(), String> {
 
     println!("{:<20} {:>4} {:<12} {:>8}", "NAME", "AGE", "CITY", "SALARY");
     println!("{}", "-".repeat(52));
-    for result in reader.deserialize() {
-        let record: Person = match result {
-            Ok(r) => r,
-            Err(e) => {
-                return Err(e.to_string());
-            }
+
+    // Read csv to vec
+    let mut people: Vec<Person> = reader
+        .deserialize()
+        .collect::<Result<Vec<Person>, _>>()
+        .map_err(|e| e.to_string())?;
+
+    // Filter
+    if let Some((filter, op, query)) = &config.filter {
+        let numeric_query = if filter == "age" || filter == "salary" {
+            Some(
+                query
+                    .parse::<u32>()
+                    .map_err(|e| format!("Error: {e} while parsing string \"{query}\" to u32"))?,
+            )
+        } else {
+            None
         };
 
-        if let Some((filter, op, query)) = &config.filter {
-            match filter.as_str() {
-                "name" => {
-                    if op.compare(record.name.to_lowercase(), query.to_lowercase()) {
-                        record.print();
-                    }
-                }
-                // First method to parse a string to u32 and returning on error
-                "age" => {
-                    if op.compare(
-                        record.age,
-                        query.parse::<u32>().map_err(|e| {
-                            format!("Error: {e} while parsing string \"{query}\" to u32")
-                        })?,
-                    ) {
-                        record.print();
-                    }
-                }
-                "city" => {
-                    if op.compare(record.city.to_lowercase(), query.to_lowercase()) {
-                        record.print();
-                    }
-                }
-                // Second method to parse a string to u32 and returning on error (explicit match)
-                "salary" => match query.parse::<u32>() {
-                    Ok(salary_query) => {
-                        if op.compare(record.salary, salary_query) {
-                            record.print();
-                        }
-                    }
-                    Err(e) => {
-                        return Err(format!(
-                            "Error: {} while parsing string \"{}\" to u32",
-                            e, query
-                        ));
-                    }
-                },
-                _ => {
-                    eprintln!("Filter target not in record headings!")
-                }
-            }
-        } else {
-            record.print();
-        }
+        people.retain(|record| match filter.as_str() {
+            "name" => op.compare(record.name.to_lowercase(), query.to_lowercase()),
+            "age" => op.compare(record.age, numeric_query.unwrap()),
+            "city" => op.compare(record.city.to_lowercase(), query.to_lowercase()),
+            "salary" => op.compare(record.salary, numeric_query.unwrap()),
+            _ => false,
+        });
+    }
+
+    // Sort
+    if let Some(sort_key) = &config.sort {
+        people.sort_by(|a, b| match sort_key.as_str() {
+            "name" => a.name.cmp(&b.name),
+            "age" => a.age.cmp(&b.age),
+            "city" => a.city.cmp(&b.city),
+            "salary" => a.salary.cmp(&b.salary),
+            _ => std::cmp::Ordering::Equal,
+        });
+    }
+
+    // Limit
+    if let Some(limit) = config.limit {
+        people.truncate(limit);
+    }
+
+    // Print resulting list
+    for person in people {
+        println!("{}", person)
     }
 
     Ok(())
