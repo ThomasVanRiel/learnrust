4c2019630e84e9bd7920d6266ec536a4b7dbf50c
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-03-04 13:12:02 +0100

    Convert to sql started

--- todo-api/src/main.rs
+++ todo-api/src/main.rs
@@ -5,7 +5,8 @@ use axum::{
     routing::get,
 };
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, Mutex};
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePool, SqlitePoolOptions};
+use std::str::FromStr;
 
 // Data model
 // ! Let the compile generate debug, clone, (de)serialize methods.
@@ -18,15 +19,21 @@ struct Todo {
 
 // Shared state
 // ! Define a new type for shorthand notation later
-type Db = Arc<Mutex<Vec<Todo>>>;
+type Db = SqlitePool;
 
 // Main function
 #[tokio::main]
-async fn main() {
-    // ! Create shared memory accessible by all threads containing a Vec which can only be accessed
-    // by one task at the time.
-    // ? What does Mutex raise when multiple tasks try to access it?
-    let db: Db = Arc::new(Mutex::new(Vec::new()));
+async fn main() -> Result<(), sqlx::Error> {
+    // ? Should I update the main() signature to return result?
+    let opts = SqliteConnectOptions::from_str("sqlite://todo.db")?.create_if_missing(true);
+
+    let pool: Db = SqlitePool::connect_with(opts).await?;
+    // Just check it the table exists or create one, we don't care about the result
+    let _ = sqlx::query(
+        "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, title TEXT, done BOOL)",
+    )
+    .execute(&pool)
+    .await;
 
     let app = Router::new()
         // ! Bind get and post calls to /todos
@@ -37,51 +44,30 @@ async fn main() {
             get(get_todo).put(update_todo).delete(delete_todo),
         )
         // ! Passes State(db) as the first argument in the REST calls
-        .with_state(db);
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on http://localhost:3000");
     axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
 
 // Handlers
 async fn list_todos(State(db): State<Db>) -> Json<Vec<Todo>> {
-    // ? Lock Mutex for lifetime of todos
-    // ? Why is unwrapping and panicking ok here?
-    let todos = db.lock().unwrap();
-    // ! Return copy of todos because db is read only
-    // ! todos can be serialized to Json because the compile derived the methods
-    Json(todos.clone())
+    todo!();
 }
 
 async fn create_todo(
     State(db): State<Db>,
     Json(payload): Json<serde_json::Value>,
 ) -> (StatusCode, Json<Todo>) {
-    let mut todos = db.lock().unwrap();
-    // ! Cast usize to u64 and increment to new index
-    let id = todos.len() as u64 + 1;
-    let todo = Todo {
-        id,
-        title: payload["title"].as_str().unwrap_or("untitled").to_string(),
-        done: false,
-    };
-    todos.push(todo.clone());
-    (StatusCode::CREATED, Json(todo))
+    todo!();
 }
 
 // ? Why does this function return Result<> while create returns tuple? Is it because GET must
 // return something while POST does not? Is the Err unwrapped by axum?
 async fn get_todo(State(db): State<Db>, Path(id): Path<u64>) -> Result<Json<Todo>, StatusCode> {
-    let todos = db.lock().unwrap();
-    todos
-        .iter()
-        .find(|t| t.id == id)
-        // ! Clone because it needs to be owned to map
-        .cloned()
-        .map(Json)
-        // ! If Err, return 404
-        .ok_or(StatusCode::NOT_FOUND)
+    todo!();
 }
 
 async fn update_todo(
@@ -89,33 +75,9 @@ async fn update_todo(
     Path(id): Path<u64>,
     Json(payload): Json<serde_json::Value>,
 ) -> Result<Json<Todo>, StatusCode> {
-    let mut todos = db.lock().unwrap();
-    // ! If id not found, return Err(404)
-    let todo = todos
-        .iter_mut()
-        .find(|t| t.id == id)
-        .ok_or(StatusCode::NOT_FOUND)?;
-    // ! Check if title is in payload and update if present
-    if let Some(title) = payload["title"].as_str() {
-        todo.title = title.to_string();
-    }
-    // ! Check if done is in payload and update if present
-    if let Some(done) = payload["done"].as_bool() {
-        todo.done = done;
-    }
-    // ! Return owned todo object
-    Ok(Json(todo.clone()))
+    todo!();
 }
 
 async fn delete_todo(State(db): State<Db>, Path(id): Path<u64>) -> StatusCode {
-    let mut todos = db.lock().unwrap();
-    let before = todos.len();
-    // Keep every todo with id mismatch
-    todos.retain(|t| t.id != id);
-    // If length is the same, the id was not deleted and hence not present in todos
-    if todos.len() < before {
-        StatusCode::NO_CONTENT
-    } else {
-        StatusCode::NOT_FOUND
-    }
+    todo!();
 }
